25bac5efb3abfeb46000382929aa7ee94c84fc58
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-30 17:35:36 +0000

    add

---
+++ my_story.py
@@ -0,0 +1,14 @@
+# my story
+
+story_1 = {
+    "start": "Ahimba woke up one day and realised her dreams were coming to reality, literally everything she saw when she was asleep was happening in real life like deja vu,",
+    "middle": "Ahimba started to predict things that might happen by remembering her dreams and they turned out to be true and she would then see these things happen",
+    "end": "Ahimba used her superpowers to protect people from danger and to tell people exciting things that were coming.",
+
+}
+
+print(story_1)
+print(type(story_1))
+print(story_1.keys())
+print(story_1.values())
+print("start")
